3168f104f6657d00a95f6195e5ff7711a3211072
Thomas Chapeaux <tc@saper-vedere.eu>
2023-10-22 03:03:47 +0200

    Add Vercel analytics

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { inject } from '@vercel/analytics'
+
 import App from './App.vue'
 
+inject() // Vercel analytics
 createApp(App).mount('#app')
